5964777a3bfc76d6a31eaf731f8972d791a4615b
folex70 <folex70@gmail.com>
2018-03-28 19:36:48 -0300

    script para movimento do player

---
+++ wheelchair-game-project/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float vel;
+	public Rigidbody2D rb;
+	public SpriteRenderer sprite;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		sprite = GetComponent<SpriteRenderer>();
+		vel = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 temp = rb.velocity;
+		temp.x = vel * Input.GetAxis("Horizontal");
+		rb.velocity = temp;
+
+		//virar 
+		if(Input.GetAxis("Horizontal") != 0){
+			
+			if(Input.GetAxis("Horizontal") > 0){
+				sprite.flipX = false;
+			}
+			else{
+
+				sprite.flipX = true;
+			}
+		}
+	}
+	
+}
